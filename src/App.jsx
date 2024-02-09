c5cfbdeab6121e050ec6caf5aa2a14364096ab65
Prashant Malviya <prashantmalviya272002@gmail.com>
2024-02-10 00:26:31 +0530

    Phone number added

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://randomuser.me/api/?page=1&results=1&seed=abc');
+        const response = await fetch('https://randomuser.me/api/?page=1&results=1&seed=abc&inc=name,picture,gender,phone');
         const result = await response.json();
         console.log(result);
         setData(result.results[0]);
@@ -23,12 +23,7 @@ function App() {
     return <p>Loading...</p>;
   }
 
-  const timestamp = data.dob?.date || '';
-  const dateObject = new Date(timestamp);
-  const year = dateObject.getFullYear();
-  const month = (dateObject.getMonth() + 1).toString().padStart(2, '0');
-  const day = dateObject.getDate().toString().padStart(2, '0');
-  const formattedDate = `${year}-${month}-${day}`;
+  const phoneNumber = data.phone || '';
 
   return (
     <a href="#" className="group flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 m-10 p-10 transition duration-300 ease-in-out transform hover:scale-105">
@@ -42,7 +37,7 @@ function App() {
           <h6 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white group-hover:text-blue-500 transition duration-300 ease-in-out">Gender: {data.gender}</h6>
         </div>
         <div className='flex flex-col'>
-          <h6 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white group-hover:text-blue-500 transition duration-300 ease-in-out">DOB: {formattedDate}</h6>
+          <h6 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white group-hover:text-blue-500 transition duration-300 ease-in-out">Phone: {phoneNumber}</h6>
         </div>
       </div>
     </a>
